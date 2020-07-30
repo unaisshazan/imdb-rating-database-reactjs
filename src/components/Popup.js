14f83aae404a2ba4d40e04f6512887dab25fe005
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-07-30 15:18:55 +0500

    add

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -7,7 +7,7 @@ function Popup({ selected, closePopup }) {
 				<h2>{ selected.Title } <span>({ selected.Year })</span></h2>
 				<p className="rating">Rating: {selected.imdbRating}</p>
 				<div className="plot">
-					<img src={selected.Poster} alt="" role="Testing bug" />
+					<img src={selected.Poster} alt=""  />
 					<p>{selected.Plot}</p>
 				</div>
 				<button className="close" onClick={closePopup}>Close</button>
--- src/components/Result.js
+++ src/components/Result.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Result({ result, openPopup }) {
 	return (
 		<div className="result" onClick={() => openPopup(result.imdbID)}>
-			<img src={result.Poster}  alt="" role="Testing bug"/>
+			<img src={result.Poster}  alt=""/>
 			<h3>{result.Title}</h3>
 		</div>
 	)
